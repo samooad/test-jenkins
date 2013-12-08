fdfd3d589ab2f8c9be22aeb88309bf1a9110207a
Samarth <samarth10612@gmail.com>
2013-12-08 17:24:52 +0530

    Modify App.java to Test CI

--- src/main/java/com/mylab/jenkins/App.java
+++ src/main/java/com/mylab/jenkins/App.java
@@ -4,7 +4,7 @@ package com.mylab.jenkins;
 public final class App {
     public static final void main(final String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Testing Jenkins");
+        System.out.println("Testing Jenkin");
     }
 }
 

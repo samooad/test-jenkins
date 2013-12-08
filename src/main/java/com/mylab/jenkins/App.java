4280f2f1dc3086afd01771e7d4128516b18bd71a
Samarth <samarth10612@gmail.com>
2013-12-08 15:20:47 +0530

    Initial Commit to start Jenkins Workflow.

---
+++ src/main/java/com/mylab/jenkins/App.java
@@ -0,0 +1,9 @@
+package com.mylab.jenkins;
+
+
+public final class App {
+    public static final void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
---
+++ src/test/java/com/mylab/jenkins/AppTest.java
@@ -0,0 +1,15 @@
+package com.mylab.jenkins;
+
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+
+public final class AppTest {
+    @Test
+    public final void app() {
+        assertTrue(true);
+    }
+}
+
